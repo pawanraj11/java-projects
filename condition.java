5294a1148187f213bf88ec1644f1374ab71d00e4
pawanraj11 <143957479+pawanraj11@users.noreply.github.com>
2023-11-01 23:45:52 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class condition {
+    public static void main(String arg[]) {
+        int age;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the age :");
+      age =sc.nextInt();
+
+        if(age>=18) {
+             System.out.println(" you are adult " +age);
+            
+             
+               }
+                else {
+                   System.out.println( " you are minor" +age);
+
+                }
+            
+
+}
+              
+               
+    }
+
